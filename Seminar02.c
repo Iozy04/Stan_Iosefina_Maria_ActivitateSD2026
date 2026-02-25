07fb1af7fe124afd934169f0c5be35976c2900aa
Iosefina <staniosefina24@stud.ase.ro>
2026-02-25 20:14:42 +0200

    Am facut functia getPrimulTelefonConditionat

--- Seminar02.c
+++ Seminar02.c
@@ -84,7 +84,16 @@ struct Telefon getPrimulElementConditionat(struct Telefon* vector, int nrElement
 	//trebuie cautat elementul care indeplineste o conditie
 	//dupa atributul de tip char*. Acesta este returnat.
 	struct Telefon t;
-	t.id = 1;
+	t.id = -1;
+	t.producator = NULL;
+	for (int i = 0; i < nrElemente; i++)
+	{
+		if (vector[i].producator != NULL && strcmp(vector[i].producator, conditie) == 0)
+		{
+			return copiazaTelefon(vector[i]);
+		}
+	}
+
 
 	return t;
 }
@@ -94,19 +103,31 @@ int main() {
 	struct Telefon t;
 	int ntTelefoaneCopiate = 2;
 	t = initializare(1, 256, "Samsung", 2000.5, 'A');
-	//afisare(t);
-	int nrTelefon = 3;
+	printf("Telefonul t este: \n");
+	afisare(t);
+	int nrTelefon = 4;
 	struct Telefon* telefoane = (struct Telefon*)malloc(sizeof(struct Telefon) * nrTelefon);
-	telefoane[0] = initializare(1,8,"Samsung",3500,'B');
-	telefoane[1] = initializare(1, 8, "Motorola", 2000.4, 'C');
-	telefoane[2] = initializare(1, 8, "Apple", 3000.4, 'D');
-	//afisareVector(telefoane, nrTelefon);
+	telefoane[0] = initializare(3, 8, "Motorola", 2000.4, 'C'); 
+	telefoane[1] = initializare(2, 8, "Google", 3500, 'B');
+	telefoane[2] = initializare(4, 8, "Apple", 3000.4, 'D');
+	telefoane[3] = t;
+	printf("Vectorul de telefoane este: \n");
+	afisareVector(telefoane, nrTelefon);
 	struct Telefon* telefoaneCopiate = copiazaPrimeleNElemente(telefoane, nrTelefon, ntTelefoaneCopiate);
-	//afisareVector(telefoaneCopiate,ntTelefoaneCopiate);
-	//dezalocare(&telefoaneCopiate,&nrTelefonCopiate);
+	printf("Primele 2 telefoane copiate sunt: \n");
+	afisareVector(telefoaneCopiate,ntTelefoaneCopiate);
+	dezalocare(&telefoaneCopiate,&ntTelefoaneCopiate);
 	int dim = 0;
 	struct Telefon* vectorNou;
-	copiazaTelefoaneScumpe(&telefoane, 3, 2000.4, &vectorNou, &dim);
+	copiazaTelefoaneScumpe(telefoane, nrTelefon, 2000.4, &vectorNou, &dim);
+	printf("Telefoane scumpe: \n");
 	afisareVector( vectorNou, dim);
+
+	struct Telefon gasit = getPrimulElementConditionat(telefoane, nrTelefon, "Samsung");
+	if (gasit.id != -1) {
+		printf("Primull telefon cu producatorul cerut este: \n");
+		afisare(gasit);
+	}
+	else printf("Nu s-a gasit niciun telefon cu producatorul cerut\n");
 	return 0;
 }
